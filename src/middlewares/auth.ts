7243a950a859c83a3a05f21c22bebc9e5935cfcf
REMIN T ROY <86014784+remintroy@users.noreply.github.com>
2023-07-05 14:51:31 +0000

    BugFix and Updated

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,18 +7,18 @@ dotenv.config();
 
 const auth = async (req: RequestDefenition, res: Response, next: NextFunction) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split(' ')?.[1];
 
     if (!token) return res.status(401).send({ success: false, message: 'Missing Token' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err: VerifyErrors | null, decoded: string | JwtPayload) => {
       if (err) return res.status(401).send({ success: false, message: 'Invalid Token' });
-      req.user = { id: decoded['id'] };
+      req.user = { id: decoded?.['id'] };
 
       next();
     });
   } catch (error) {
-    console.error('Error in auth midleware :-', error);
+    console.error('Error in auth midleware :- ', error);
     res.status(500).send({ success: false, message: 'Internal Server Error' });
   }
 };
